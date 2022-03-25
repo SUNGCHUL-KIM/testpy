10c8930c81476ae864684b5e353c338c48782fd8
SUNGCHUL-KIM <102336838+SUNGCHUL-KIM@users.noreply.github.com>
2022-03-25 11:53:43 +0900

    Add files via upload

---
+++ 3번 과제.py
@@ -0,0 +1,5 @@
+data = [32,14,55,23,12,67,44]
+
+for d in data :
+    if d<20 or d>=50 :
+        print(d,end=',')
---
+++ 5번 과제.py
@@ -0,0 +1,9 @@
+data=[32,14,55,23,12,67,44]
+n=0
+
+for d in data :
+    if d>=30 :
+        n=n+1
+
+print(n)
+

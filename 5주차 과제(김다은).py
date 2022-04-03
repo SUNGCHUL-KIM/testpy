5e52c7835bdb6e511a5e0bd9dd8be3f829931939
SUNGCHUL-KIM <102336838+SUNGCHUL-KIM@users.noreply.github.com>
2022-04-03 20:56:51 +0900

    Add files via upload

---
+++ 5주차 과제(김다은).py
@@ -0,0 +1,9 @@
+# 김다은 문제
+
+s='CLASS JAVA'
+
+new_s = s[0:1]+'lass python'
+
+
+print(new_s)
+print(new_s[7])
---
+++ 5주차 과제(박종현).py
@@ -0,0 +1,12 @@
+# 박종현 문제 1번
+
+sentence = 'I hope you have a good day'
+
+print(len(sentence))
+
+#문제 2
+
+
+s = 'Today I learned'
+
+print(s[0:5],',',s[6:7],',',s[8:])

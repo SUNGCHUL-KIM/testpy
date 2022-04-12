6dbe80913f98b5f72b4db99e771d04c85d4e3438
SUNGCHUL-KIM <102336838+SUNGCHUL-KIM@users.noreply.github.com>
2022-04-12 22:10:55 +0900

    Add files via upload

---
+++ 6주차 과제.py
@@ -0,0 +1,12 @@
+# 배열 안의 숫자들에서 합계, 최대, 최소값 구하는 문제
+
+Arr=[35, 10, 30, 20, 40, 90, 45, 56, 24]
+
+# 모든 배열의 합
+print("sum of ArrayList : ",sum(Arr))
+
+# 배열 안의 수 중 최대값
+print("max value of ArrayList :",max(Arr))
+
+# 배열 안의 수 중 최소값
+print("min value of ArrayList :",min(Arr))
